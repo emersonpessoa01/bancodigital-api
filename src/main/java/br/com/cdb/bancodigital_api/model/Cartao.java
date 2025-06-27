52b2a49e185a293b1e23d16e41f0c180aa44ea73
emersonpessoa <emersonpessoa05102008@gmail.com>
2025-06-27 16:18:03 -0300

    feat: Started Cartao :sparkles:

---
+++ src/main/java/br/com/cdb/bancodigital_api/model/Cartao.java
@@ -0,0 +1,25 @@
+package br.com.cdb.bancodigital_api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "cartoes")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Cartao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String numero;
+    private String bandeira;
+    private String tipo; // Ex: "Crédito", "Débito", "Múltiplo"
+    private Double limite;
+
+    @ManyToOne
+    @JoinColumn(name = "conta_id")
+    private Conta conta; // Associação com a conta bancária a que o cartão pertence
+}
