950044f81fb03b2dc8fc08591ccf5cd7d6b5072f
emersonpessoa <eps011108@gmail.com>
2025-07-17 17:23:31 -0300

    feat: Added FaturaDTO :rocket:

--- src/main/java/br/com/cdb/bancodigital_api/dto/FaturaDTO.java
+++ src/main/java/br/com/cdb/bancodigital_api/dto/FaturaDTO.java
@@ -1,12 +1,14 @@
 package br.com.cdb.bancodigital_api.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Builder // ✅ ESSA ANOTAÇÃO ESTÁ FALTANDO
 public class FaturaDTO {
     private Long cartaoId;
     private Double total;
-    private List<LancamentoFaturaDTO> lancamentos;
+    private List<LancamentoDTO> lancamentos;
 }
