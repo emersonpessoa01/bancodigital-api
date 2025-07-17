dfd24675bbad2263f7529013a023702c884ae19b
emersonpessoa <eps011108@gmail.com>
2025-07-17 15:22:41 -0300

    feat: Added field status in CartaoDTO :rocket:

--- src/main/java/br/com/cdb/bancodigital_api/dto/CartaoDTO.java
+++ src/main/java/br/com/cdb/bancodigital_api/dto/CartaoDTO.java
@@ -9,12 +9,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class CartaoDTO
-{
+public class CartaoDTO {
     private Long id;
     private String numero;
     private String bandeira;
-    private String tipo; // Ex: "Crédito", "Débito", "Múltiplo"
+    private String tipo; // Ex: "Crédito", "Débito"
     private Double limite;
     private Long contaId; // ID da conta bancária a que o cartão pertence
+    private Boolean status; // novo campo adicionado
 }
+
