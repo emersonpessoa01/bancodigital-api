5aa72248d7c77f14edeb69f8759411be4f799d29
emersonpessoa <eps011108@gmail.com>
2025-07-17 12:37:39 -0300

    feat: Adjusted GlobalExceptionHandler :rocket:

--- src/main/java/br/com/cdb/bancodigital_api/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/cdb/bancodigital_api/exception/GlobalExceptionHandler.java
@@ -1,25 +1,21 @@
 package br.com.cdb.bancodigital_api.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-import java.util.Map;
-
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<?> handlerNotFound(ResourceNotFoundException ex){
-        return ResponseEntity.status(404).body(Map.of(
-                "erro", ex.getMessage(),
-                "timestamp", LocalDateTime.now()
-        ));
+    public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> handlerException(Exception ex){
-        return ResponseEntity.status(500).body(Map.of(
-                "erro", "Ocorreu um erro inesperado do servidor",
-                "detalhes", ex.getMessage(),
-                "timestamp", LocalDateTime.now()
-        ));
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArg(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
+
+    // Outros tratamentos customizados se necessário
 }
