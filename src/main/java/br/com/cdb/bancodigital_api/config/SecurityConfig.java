f1a81405a1b6543564681303d88c353a0cdfb700
emersonpessoa <eps011108@gmail.com>
2025-07-18 10:51:00 -0300

    fix: Ajusted SecurityConfig :wrench:

--- src/main/java/br/com/cdb/bancodigital_api/config/SecurityConfig.java
+++ src/main/java/br/com/cdb/bancodigital_api/config/SecurityConfig.java
@@ -11,15 +11,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.disable()) // desativa CSRF completamente
-                .headers(headers -> headers.frameOptions(frame -> frame.disable())) // permite H2
+                .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frame -> frame.disable()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers("/clientes/**").permitAll()
                         .requestMatchers("/contas/**").permitAll()
                         .requestMatchers("/cartoes/**").permitAll()
-                        .anyRequest().authenticated() // exige autenticação nos demais
+                        .anyRequest().permitAll()
                 )
-                .build();
+                .build(); // <- Faltava o build aqui
     }
 }
