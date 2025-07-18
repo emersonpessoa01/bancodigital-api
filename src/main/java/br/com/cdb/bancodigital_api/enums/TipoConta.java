e6fd03055566adb0d2365c270581e62c00a95de3
emersonpessoa <eps011108@gmail.com>
2025-07-18 10:51:43 -0300

    fix: Adjusted TipoConta :wrench:

--- src/main/java/br/com/cdb/bancodigital_api/enums/TipoConta.java
+++ src/main/java/br/com/cdb/bancodigital_api/enums/TipoConta.java
@@ -1,6 +1,12 @@
 package br.com.cdb.bancodigital_api.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum TipoConta {
-    CORRENTE,
-    POUPANCA
+    CORRENTE, POUPANCA;
+
+    @JsonCreator
+    public static TipoConta from(String value) {
+        return TipoConta.valueOf(value.toUpperCase());
+    }
 }
